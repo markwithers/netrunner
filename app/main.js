14a0846eedfea31eaaecb317adbe751d2997c07e
markwithers <mark26266@gmail.com>
2014-01-23 16:58:40 +0000

    Think I'm gonna use Sammy

--- app/main.js
+++ app/main.js
@@ -1,4 +1,21 @@
 var player = require('./models/player')
 
-mark = player('mark');
-alert(JSON.stringify(mark));
+var app = Sammy('#main', function() {
+  
+  this.get('#path', function() {
+    this.$element().html('A new route!');
+
+    mark = player('marcus');
+    alert(JSON.stringify(mark));
+  });
+
+  this.get('#test', function() { 
+    alert('test');
+  });
+
+});
+
+$(function() {
+  app.run();
+});
+
---
+++ app/presenters/players.js
@@ -0,0 +1,7 @@
+module.exports = function(){
+  return {
+    doThing: function() {
+      alert('thing');
+    }
+  }
+}()
